80d4ac214954e707a3a05bbd9e85f9fd40f62cc0
wadhah <wadhah.mahroug15@gmail.com>
2021-11-28 20:21:57 +0100

    setup ngxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
+// import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    NgxsRouterPluginModule.forRoot(),
+    NgxsModule.forRoot([], {
+      developmentMode: true,
+    }),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
