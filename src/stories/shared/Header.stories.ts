5480ef1ead7486e03a31ad32511b1f2d958383b4
wadhah <wadhah.mahroug15@gmail.com>
2021-12-22 09:56:27 +0100

    added colors

--- src/stories/shared/Header.stories.ts
+++ src/stories/shared/Header.stories.ts
@@ -19,4 +19,3 @@ const Template: Story<HeaderComponent> = (args: HeaderComponent) => ({
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/angular/writing-stories/args
-Primary.args = {};
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   purge: ["./src/**/*.{html,ts}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#FFDFC8",
+          normal: "#ffa934",
+        },
+        secondary: {
+          normal: "#b8dfd8",
+          light: "#E8F6EF",
+          lighter: "#F8F8FA",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
