3e6cbeb28bd1c2f1be5cafd6192aa0544a1b2e6e
wadhah <wadhah.mahroug15@gmail.com>
2021-12-26 18:17:53 +0100

    tailwind dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
