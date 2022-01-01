8f260ece20fa65f9d648c94f50a643d9feaa89c2
Tess99854 <stesnime@yahoo.com>
2022-01-01 17:28:06 +0100

    fix buttons

--- src/app/components/forms/generic-form/generic-form.component.ts
+++ src/app/components/forms/generic-form/generic-form.component.ts
@@ -1,7 +1,6 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Field} from '../../../models/Field';
-
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Field } from '../../../models/Field';
 
 @Component({
   selector: 'app-generic-form',
@@ -10,34 +9,33 @@ import {Field} from '../../../models/Field';
 })
 export class GenericFormComponent implements OnInit {
   @Input() form: Field[] = [];
-  @Input() crossValidators: Validators[] = []
-  @Input() primaryMode: boolean = true ;
+  @Input() crossValidators: Validators[] = [];
+  @Input() primaryMode: boolean = true;
   @Input() isLoading = false;
   @Input() formErrors: string[] = [];
 
   @Output() onSubmit = new EventEmitter<any>();
 
   formGroup: FormGroup | undefined;
-  double: boolean = false
+  double: boolean = false;
   controlsErrorMessages = {
-    'required': 'This field is required',
-    'email': 'Please enter a valid email',
-    'minlength': 'This field requires at least 8 characters',
-    'password_pattern':  'Should contain one uppercase, one lower case and a number',
-    'full_name_pattern': 'Please enter both your first and last name',
-  }
+    required: 'This field is required',
+    email: 'Please enter a valid email',
+    minlength: 'This field requires at least 8 characters',
+    password_pattern: 'Should contain one uppercase, one lower case and a number',
+    full_name_pattern: 'Please enter both your first and last name',
+  };
 
   groupErrorMessages = {
-    'confirm_password': {message: 'Passwords don\'t match', position: 'password confirm'},
-  }
+    confirm_password: { message: "Passwords don't match", position: 'password confirm' },
+  };
 
-  constructor(private fb: FormBuilder) {
-  }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.double = this.form.length > 3
+    this.double = this.form.length > 3;
     let config = {};
-    let options = {validator: this.crossValidators}
+    let options = { validator: this.crossValidators };
 
     for (let field of this.form) {
       // @ts-ignore
@@ -45,29 +43,28 @@ export class GenericFormComponent implements OnInit {
     }
 
     this.formGroup = this.fb.group(config, options);
-
   }
 
   getControl(name: string) {
     return this.formGroup.get(name);
   }
 
-  getControlErrors(name: string){
-    if (this.formGroup.get(name).errors) return Object.keys(this.formGroup.get(name).errors)
-    return []
+  getControlErrors(name: string) {
+    if (this.formGroup.get(name).errors) return Object.keys(this.formGroup.get(name).errors);
+    return [];
   }
 
   getGroupErrors() {
-    if (this.formGroup.errors) return Object.keys(this.formGroup.errors)
-    return []
+    if (this.formGroup.errors) return Object.keys(this.formGroup.errors);
+    return [];
   }
 
   isErrorField(name: string) {
-    let errors = this.getGroupErrors()
-    for(let error of errors) {
-      if (this.groupErrorMessages[error].position == name) return true
+    let errors = this.getGroupErrors();
+    for (let error of errors) {
+      if (this.groupErrorMessages[error].position == name) return true;
     }
-    return false
+    return false;
   }
 
   submit() {
