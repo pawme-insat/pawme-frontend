96929542dc727100e671b889cca33219dbbef029
wadhah <wadhah.mahroug15@gmail.com>
2022-01-01 19:36:52 +0100

    srp

--- src/app/directive/clicked-outside/clicked-outside.directive.ts
+++ src/app/directive/clicked-outside/clicked-outside.directive.ts
@@ -4,21 +4,15 @@ import { Directive, ElementRef, EventEmitter, HostListener, Input, Output } from
   selector: '[appClickedOutside]',
 })
 export class ClickedOutsideDirective {
-  @Output() clickOutside = new EventEmitter<void>();
-
-  @Input() clickExceptions: ElementRef[] = [];
+  @Output() clickOutside = new EventEmitter<ElementRef>();
 
   constructor(private elementRef: ElementRef) {}
 
   @HostListener('document:click', ['$event.target'])
   public onClick(target) {
-    if (this.clickExceptions.find((e) => e === target)) {
-      console.log('target exception');
-      return;
-    }
     const clickedInside = this.elementRef.nativeElement.contains(target);
     if (!clickedInside) {
-      this.clickOutside.emit();
+      this.clickOutside.emit(target);
     }
   }
 }
