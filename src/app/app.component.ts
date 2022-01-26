b29c6155562d1916e3b83260419799f4bab15e4b
wadhah <wadhah.mahroug15@gmail.com>
2022-01-26 10:04:03 +0100

    added modal logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Select } from '@ngxs/store';
+import { filter, map, Observable, of, switchMap, tap } from 'rxjs';
+import { User } from './services/pawme.graphql.service';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +11,20 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   title = 'pawme-frontend';
+  @Select((e) => e.auth.user)
+  user: Observable<User>;
+
+  $showAddPetModal: Observable<Boolean>;
 
   noFooter = ['/sign-up', '/sign-in', '/profile', '/pet-profile', '/add-pet'];
 
   noNavbar = ['/sign-up', '/sign-in', '/add-pet'];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.$showAddPetModal = this.showAddPetModal();
+  }
 
   has_footer(): boolean {
     return this.has_layout(this.noFooter);
@@ -27,5 +36,28 @@ export class AppComponent implements OnInit {
 
   has_layout(routes: string[]): boolean {
     return !routes.find((e) => e === this.router.url);
+  }
+
+  showAddPetModal(): Observable<Boolean> {
+    return of(false);
+
+    const allowedUrls = ['/add-pet'];
+
+    const currentRoute = this.router.events.pipe(
+      filter((event: NavigationEnd) => event instanceof NavigationEnd),
+      map((e) => e.url)
+    );
+
+    return currentRoute.pipe(
+      switchMap((url) =>
+        this.user.pipe(
+          map((user) => {
+            if (user === null) return false;
+            if (allowedUrls.find((u) => u === url)) return false;
+            return user.pets.length == 0;
+          })
+        )
+      )
+    );
   }
 }
